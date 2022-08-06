58baf20f65ae05a57dbaf8ecc7eef522a0970803
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-08-06 12:25:34 -0300

    feat: criação do componente de pesquisa do menu

--- app/src/main/java/br/com/portfolio/app/repositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/portfolio/app/repositories/ui/MainActivity.kt
@@ -1,10 +1,18 @@
 package br.com.portfolio.app.repositories.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import br.com.portfolio.app.repositories.R
 import br.com.portfolio.app.repositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    companion object {
+        private const val TAG = "TAG"
+    }
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -15,4 +23,20 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "onQueryTextSubmit: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return false
+    }
 }
